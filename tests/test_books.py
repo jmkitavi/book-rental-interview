79c75b09a066ff16bec806586c43a71767d25968
jmkitavi <kitavimjoseph@gmail.com>
2020-12-16 10:54:04 +0300

    add story 2 tests

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,5 +1,5 @@
 import unittest
-from app import Story1
+from app import Story1, Story2
 
 class TestBookCalculation(unittest.TestCase):
   def test_story_one(self):
@@ -8,5 +8,14 @@ class TestBookCalculation(unittest.TestCase):
     per_day_rental = 1
     self.assertEqual(Story1(number_of_books, duration).calculate_charge(per_day_rental), 6)
 
+
+  def test_story_two(self):
+    number_of_books = 2
+    duration = 3
+
+    self.assertEqual(Story2(number_of_books,duration, 'regular').calculate_charge(), 9)
+    self.assertEqual(Story2(number_of_books,duration,'fiction').calculate_charge(), 18)
+    self.assertEqual(Story2(number_of_books,duration, 'novel').calculate_charge(), 9)
+    
 if __name__ == '__main__':
     unittest.main()

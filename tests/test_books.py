ff8376864ca47b2a763f536fe421add437d15e71
jmkitavi <kitavimjoseph@gmail.com>
2020-12-16 10:57:46 +0300

    add test for story 3

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,5 +1,5 @@
 import unittest
-from app import Story1, Story2
+from app import Story1, Story2, Story3
 
 class TestBookCalculation(unittest.TestCase):
   def test_story_one(self):
@@ -13,9 +13,18 @@ class TestBookCalculation(unittest.TestCase):
     number_of_books = 2
     duration = 3
 
-    self.assertEqual(Story2(number_of_books,duration, 'regular').calculate_charge(), 9)
-    self.assertEqual(Story2(number_of_books,duration,'fiction').calculate_charge(), 18)
-    self.assertEqual(Story2(number_of_books,duration, 'novel').calculate_charge(), 9)
+    self.assertEqual(Story2(number_of_books, duration, 'regular').calculate_charge(), 9)
+    self.assertEqual(Story2(number_of_books, duration,'fiction').calculate_charge(), 18)
+    self.assertEqual(Story2(number_of_books, duration, 'novel').calculate_charge(), 9)
     
+  def test_story_three(self):
+    number_of_books = 2
+    duration = 3
+
+    self.assertEqual(Story3(number_of_books, duration, 'regular').calculate_charge(),7)
+    self.assertEqual(Story3(number_of_books, duration, 'fiction').calculate_charge(),18)
+    self.assertEqual(Story3(number_of_books, duration, 'novel').calculate_charge(),27)
+
+
 if __name__ == '__main__':
     unittest.main()
